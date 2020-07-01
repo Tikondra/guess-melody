b8972892fd4d957cb23e277b4174d06cc61dc07d
Tikondra <tikondra47@gmail.com>
2020-07-01 20:21:51 +0400

    Добавляет Redax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import {Settings} from "./constants";
 import questions from "./mocks/questions";
+import {reducer} from "./reducer.js";
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 const init = () => {
   ReactDOM.render(
-      <App
-        questions = {questions}
-        errorsCount = {Settings.ERROR_COUNT}
-        gameTime = {Settings.GAME_TIME}
-      />,
+      <Provider store={store}>
+        <App
+          questions = {questions}
+          errorsCount = {Settings.ERROR_COUNT}
+          gameTime = {Settings.GAME_TIME}
+        />
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
